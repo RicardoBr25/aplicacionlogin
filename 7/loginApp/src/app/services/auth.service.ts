a52182b9b3bc76ea1ea0982d1370b1713c84c4c2
ricardobr25 <113959643+RicardoBr25@users.noreply.github.com>
2023-11-08 07:39:43 -0500

    Video 11

---
+++ 7/loginApp/src/app/services/auth.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UsuarioModel } from '../models/usuario.modelo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url = 'https://identitytoolkit.googleapis.com/v1/accounts:';
+  private apikey = 'AIzaSyDNnSFX4FiKtVGpN_1kFiCQc3H7YQmlxs0';
+
+
+  // CREAR NUEVOS USUARIOS
+  // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+
+
+  // Login
+  // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+
+
+  constructor( private http:HttpClient ) { }
+
+  logout() { 
+
+  }
+
+  login( usuario: UsuarioModel ) { 
+
+  }
+
+  nuevoUsuario( usuario: UsuarioModel ) {
+
+  }
+}
